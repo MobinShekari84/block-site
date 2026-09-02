253f8aa3b451385e893850afc514195664c2401a
Mobin Shekari <mobin.shekari84@gmail.com>
2026-09-02 15:41:20 +0330

    chore: clean up temp scripts

--- update_config.py
+++
@@ -1,9 +0,0 @@
-import re
-with open('js/damas-villa.js', 'r') as f:
-    js = f.read()
-
-if 'heroTheme' not in js:
-    js = js.replace("id: 'damas-villa',", "id: 'damas-villa',\n  heroTheme: 'light', // 'light' means the image is light, so text should be dark")
-
-with open('js/damas-villa.js', 'w') as f:
-    f.write(js)
